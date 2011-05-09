d871f7b208a558488db7c1c084825f1341c977be
Marek Ryniejski <marek@MarekJulianPowerMacG5.local>
2011-05-08 21:22:01 -0700

    - fix sorting of images in collection

--- app/models/image.rb
+++ app/models/image.rb
@@ -7,4 +7,19 @@ class Image < ActiveRecord::Base
 
     accepts_nested_attributes_for :image_variants, :allow_destroy => true
 
+    include Comparable
+
+    def <=>(other)
+        logger.debug "image - comparing #{self.id} with #{other.id}!"
+        if self.master_variant_id and other.master_variant_id
+            master = ImageVariant.find( self.master_variant_id )
+            other_master = ImageVariant.find( other.master_variant_id )
+            logger.debug "image - comparing masters: #{master.file_file_name} with #{other_master.file_file_name}!"
+            master.file_file_name<=>other_master.file_file_name
+        else
+            self.image_variants.first<=>other.image_variants.first
+        end
+
+    end
+
 end
